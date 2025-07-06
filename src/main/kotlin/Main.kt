48c7ec6a43466bb30123d7288e364863a3c01e2b
Nikil Sri Shen R <nikilsrishen@gmail.com>
2025-07-06 12:44:17 +0530

    Hello World Program

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello, World!")
+
+    // This is a simple Kotlin program that prints "Hello, World!" to the console.
+
+    // Single-line comment
+
+    /*
+    Multi-line comment
+     */
+
+    // You can run this program using a Kotlin compiler or an IDE that supports Kotlin.
+}
